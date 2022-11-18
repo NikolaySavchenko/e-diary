81e2b469088257c55242eb080db9625c3f639d02
Nikolay Savchenko <7548568@gmail.com>
2022-11-18 21:54:15 +0300

    Removed sekret_key

--- project/settings.py
+++ project/settings.py
@@ -7,9 +7,9 @@ env.read_env()
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', default=True)
 
 DATABASES = {
     'default': {
