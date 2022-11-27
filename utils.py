61d301e3731fc4d7686e23bc6771068647079607
Nikolay Savchenko <7548568@gmail.com>
2022-11-27 11:02:51 +0300

    Add function get_schoolkid

--- utils.py
+++ utils.py
@@ -8,34 +8,30 @@ from datacenter.models import Subject
 from datacenter.models import Commendation
 
 
-def fix_marks(schoolkid_name):
+def get_schoolkid(schoolkid_name):
     client_card = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
     if len(client_card)>1:
         return f'Найдено {len(client_card)} учеников!'
     elif client_card.first() is None:
         return 'Не найдено ни одного ученика с подходящим именем!'
-    client_marks = Mark.objects.filter(schoolkid=client_card.first().id)
+    return client_card.first()
+
+def fix_marks(schoolkid_name):
+    client_card = get_schoolkid(schoolkid_name)
+    client_marks = Mark.objects.filter(schoolkid=client_card.id)
     client_marks.filter(points__lt=4).update(points=5)
     return 'Все ок!'
 
 def remove_chastisement(schoolkid_name):
-    client_card = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
-    if len(client_card)>1:
-        return f'Найдено {len(client_card)} учеников!'
-    elif client_card.first() is None:
-        return 'Не найдено ни одного ученика с подходящим именем!'
-    client_chastisement = Chastisement.objects.filter(schoolkid=client_card.first().id)
+    client_card = get_schoolkid(schoolkid_name)
+    client_chastisement = Chastisement.objects.filter(schoolkid=client_card.id)
     client_chastisement.delete()
     return 'Все ок!'
 
 def create_commendation(schoolkid_name, subject):
-    client_card = Schoolkid.objects.filter(full_name__contains=schoolkid_name)
-    if len(client_card)>1:
-        return f'Найдено {len(client_card)} учеников!'
-    elif client_card.first() is None:
-        return 'Не найдено ни одного ученика с подходящим именем!'
-    lessons = Lesson.objects.filter(year_of_study=client_card.first().year_of_study,
-                                    group_letter=client_card.first().group_letter, subject__title=subject)
+    client_card = get_schoolkid(schoolkid_name)
+    lessons = Lesson.objects.filter(year_of_study=client_card.year_of_study,
+                                    group_letter=client_card.group_letter, subject__title=subject)
     compliments = [
         'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
         'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
@@ -48,8 +44,8 @@ def create_commendation(schoolkid_name, subject):
     ]
     random_compliment = choice(compliments)
     lesson = choice(lessons.reverse()[:5])
-    subject_card = Subject.objects.filter(title__contains=subject, year_of_study__contains=lesson.year_of_study)[0]
-    Commendation.objects.create(text=random_compliment, created=lesson.date, schoolkid=client_card.first(),
+    subject_card = Subject.objects.filter(title__contains=subject, year_of_study__contains=lesson.year_of_study).first()
+    Commendation.objects.create(text=random_compliment, created=lesson.date, schoolkid=client_card,
                                 subject=subject_card, teacher=lesson.teacher)
     return 'Все ок!'
 
